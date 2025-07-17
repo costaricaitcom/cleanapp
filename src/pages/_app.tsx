213736fb1c570597f85429e4787190fb786b7dd4
costaricaitcom <costaricaitcom@gmail.com>
2025-07-17 15:04:46 -0600

    fix: importa CSS global de calendario en _app.tsx y corrige error de sintaxis

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { AuthProvider } from "../context/AuthContext";
 import Layout from "../components/Layout";
 import "../styles/global.css";
+import '../styles/date-range-dark.css';
 
 export default function App({ Component, pageProps }) {
   return (
